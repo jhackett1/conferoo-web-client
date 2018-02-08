3f90ead32226aaa70587b50b03458f6998090856
jhackett1 <joshua.hackett93@gmail.com>
2018-02-08 10:05:43 +0000

    Eurgh

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ const config = {
   google_client_id: "826165205387-b4u0mtq8diena38nabgqrd667kefjnfr.apps.googleusercontent.com",
   api_host: "https://www.fsconference.co.uk/api/",
   slack_webhook_url: "https://hooks.slack.com/services/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",
-  // login_redirect_uri: 'http://localhost:3000/login/callback',
+  login_redirect_uri: 'http://localhost:3000/login/callback',
   // login_redirect_uri: 'https://conferoo-web-client.herokuapp.com/login/callback',
-  login_redirect_uri: 'https://app.fsconference.co.uk/login/callback',
+  // login_redirect_uri: 'https://app.fsconference.co.uk/login/callback',
   themes: [
     "Understanding the UK",
     "Policy and public engagement",
     "Data and technology"
   ],
   venues: [
-    'C11', 'C6/34', 'C8/9', 'C20'
+    'C3', 'C8/9', 'C11', 'C20', 'C6/34'
   ],
   // Extra events to put in the timetable
   decoration: [{
